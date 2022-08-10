ce4cde7dd2a0383dcfd43d15206cb734d154ca20
Tiago Tavares <tiago.tavares@inesctec.pt>
2022-08-10 16:56:23 +0100

    completes auth basics

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,6 @@
-import { testRoute } from './testRoute';
+import { signUpRoute } from "./signUpRoute";
+import { testRoute } from "./testRoute";
+import { logInRoute } from "./loginRoute";
+import { updateUserInfoRoute } from "./updateUserRoute";
 
-export const routes = [
-    testRoute,
-];
+export const routes = [updateUserInfoRoute, logInRoute, signUpRoute, testRoute];
