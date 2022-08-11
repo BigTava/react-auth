8f24faf6f7e11f4c3b4aab2436e14651cd9606fd
Tiago Tavares <tiago.tavares@inesctec.pt>
2022-08-11 10:41:25 +0100

    starts password reset feature

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -4,8 +4,10 @@ import { logInRoute } from "./loginRoute";
 import { updateUserInfoRoute } from "./updateUserRoute";
 import { testEmailRoute } from "./testEmailRoute";
 import { verifyEmailRoute } from "./verifyEmailRoute";
+import { forgotPasswordRoute } from "./forgotPasswordRoute";
 
 export const routes = [
+  forgotPasswordRoute,
   verifyEmailRoute,
   updateUserInfoRoute,
   logInRoute,
